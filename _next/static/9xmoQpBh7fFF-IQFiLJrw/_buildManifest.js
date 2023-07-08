f7a45c1b0482bf27f2bd367a12bf52afee331950
Xnork <xnorkdeuzivan@gmail.com>
2023-07-08 09:14:29 -0300

    upload site

---
+++ _next/static/9xmoQpBh7fFF-IQFiLJrw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/389-596ea52217eb1138.js","static/css/07a80596056a15fe.css","static/chunks/pages/index-cad155df92180ee9.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- components/ActionBar/index.js
+++
@@ -1,9 +0,0 @@
-import styles from "./style.module.scss"
-
-export const ActionBar = () => {
-  return (
-    <nav className={styles.container}>
-      <h1>Portfolio</h1>
-    </nav>
-  )
-}
